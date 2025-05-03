45e42689c95ab372f6d8507bbc1870b09630fc39
Lexi <felixpinson@unicaen.fr>
2025-05-03 21:47:37 +0200

    CheckStyle et converture de +90% moyen (95% d'instruction / 87% de branche)

--- src/test/java/simulateurReusiner/TestsSimulateurReusiner.java
+++ src/test/java/simulateurReusiner/TestsSimulateurReusiner.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestsSimulateurReusiner {
 
@@ -222,6 +224,5 @@ public class TestsSimulateurReusiner {
 
         // Assert
         assertEquals(   Integer.valueOf(impotAttendu), simulateur.getImpotSurRevenuNet());
-    }
-
+    }    
 }
